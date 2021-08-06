25a838b9a6b2d3a7d71cefe256c4f6c53ab87a37
Sarah Coulson <scoulson2023@gmail.com>
2021-08-05 22:46:42 -0500

    rearranging fs setup and adding js file

---
+++ server.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const database = require('./db/db.json')
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
