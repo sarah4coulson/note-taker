d38f59f072e1a618e07d16fe18802579fc2e1319
Sarah Coulson <scoulson2023@gmail.com>
2021-08-05 23:44:30 -0500

    additon of post function

--- server.js
+++ server.js
@@ -1,30 +1,41 @@
-const path = require('path');
-const fs = require('fs');
-const express = require('express');
-const database = require('./db/db.json')
-
+const path = require("path");
+const fs = require("fs");
+const express = require("express");
+const noteDatabase = require("./db/db.json");
 const app = express();
-
 const PORT = process.env.PORT || 3001;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.use(express.static('public'));
 
-
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
-app.get('/api/notes', (req, res) => {
-    res.json(database);
+
+app.get("/api/notes", (req, res) => {
+  res.json(noteDatabase);
 });
 
+
+app.post("/api/notes", (req, res) => {
+  noteDatabase.push(req.body);
+  noteDatabase.forEach((obj, i) => {
+    obj.id = i + 1;
+  });
+
+  fs.writeFile("./db/db.json", JSON.stringify(noteDatabase), () => {
+    res.json(noteDatabase);
+  });
+});
 
 
 app.listen(PORT, () =>
