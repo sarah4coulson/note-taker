68c7502e6bc84016e06ea1b51ca239b574e09c9e
Sarah Coulson <scoulson2023@gmail.com>
2021-08-05 23:27:01 -0500

    editing the server.js file

--- server.js
+++ server.js
@@ -12,3 +12,21 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+    res.json(database);
+});
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
